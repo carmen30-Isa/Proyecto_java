f0fbe9e8c17eeb004cd5dd9117b833259b7728af
carmen30-Isa <82846294+carmen30-Isa@users.noreply.github.com>
2021-05-08 17:25:50 -0600

    Add files via upload

---
+++ GitHub/src/github/GitHub.java
@@ -0,0 +1,21 @@
+
+package github;
+
+/**
+ *
+ * @author Carmen_Mejía
+ */
+public class GitHub {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Proyecto_java");
+        System.out.println("------------------------");
+        System.out.println("Ejercicio #2");
+        System.out.println("-------------------------");
+        System.out.println("Carmen Mejía");
+        
+    }
+    
+}
